6bcffff0414745c2213b2642a0eb770eea9c9d86
sezaiemrekonuk <sezaikonuk@hacettepe.edu.tr>
2024-07-01 11:51:28 +0300

    deleted files

--- b2220765026/src/Store.java
+++
@@ -1,4 +0,0 @@
-// A common interface for store objects, there isn't any methods yet.
-// This is just a placeholder for future use.
-// It is used to store the store objects in a list.
-public interface Store { }
